0af4dd54e799fcd14fb7416685a83e37411db7fd
farhanahmed01 <Farhanahmed51140@gmail.com>
2025-02-18 17:13:46 +0530

    ss

--- src/components/A.js
+++ src/components/A.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+
+import { Link } from 'react-router-dom';
+import React , {useState}  from 'react'
 
 
-export default function Navbar(props) {
+
+const Navbar = (props) => {
+    const [isHovered, setIsHovered] = useState(false);
+    
     return ( 
         <div >
         <nav className={`navbar navbar-expand-lg navbar-${props.Mode} bg-${props.Mode}`}>
         <div className="container-fluid">
-        <Link className="navbar-brand" to="/">TxtFix</Link>
+        <Link ClassName="navbar-brand" to="/">TxtFix</Link>
+
 
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
@@ -15,17 +20,39 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+            <Link ClassName="nav-link active" aria-current="page">Convert Case</Link>
 
             </li>
             <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/About">About</Link>
+            <Link ClassName="nav-link active" aria-current="page" to ="/About">About</Link>
 
             </li>
-            <li className="nav-item">
-            <Link className="nav-link" to="/">Link</Link>
-            </li>
+                   
+            <li class="nav-item dropdown" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+          <a class="nav-href dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Text Modification
+          </a>
+          
+          <ul class="dropdown-menu" style={{ display: isHovered ? "block" : "none"}}>
+            <li><a class="dropdown-item" href="/BigText">Big Text Generator</a></li>
+            <li><a class="dropdown-item" href="/">Bold Text Generator</a></li>
+            <li><a class="dropdown-item" href="/">Duplicate Line Remover</a></li>
+            <li><a class="dropdown-item" href="/">Reverse Text Generator</a></li>
+          </ul>
+        </li>
 
+        <li className="nav-item">
+            <Link ClassName="nav-link active" aria-current="page" to="/">Image tool</Link>
+
+        </li>
+            
+        
+       
+        <li className="nav-item">
+            <Link ClassName="nav-link active" aria-current="page" to="/">Random Generator</Link>
+
+        </li>
+            
                 
         </ul>
             
@@ -42,3 +69,5 @@ export default function Navbar(props) {
     </div>
   )
 }
+
+export default Navbar;
